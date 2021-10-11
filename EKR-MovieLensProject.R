0fc3efd43521f10c6d98b120ed4ed1fd73574b76
EKRihani <emir.kais@rihani.eu>
2021-10-11 16:15:24 +0200

    11 oct 16:15

--- EKR-MovieLensProject.R
+++ EKR-MovieLensProject.R
@@ -22,19 +22,19 @@ library(data.table)
 dl <- tempfile()
 #download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl) # Download Remote File
 
-dl <- "~/projects/movielens/ml-10m.zip"    # Use Local File (faster)
-ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
-                 col.names = c("userId", "movieId", "rating", "timestamp"))
-movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
+#dl <- "~/projects/movielens/ml-10m.zip"    # Use Local File (faster)
+#ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
+#                 col.names = c("userId", "movieId", "rating", "timestamp"))
+#movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
 
 ##### DEBUT A ENLEVER ####
 #dl <- "~/projects/movielens/ml-20m.zip"   # /!\ GROS dataset
 #ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-20m/ratings.csv"))), col.names = c("userId", "movieId", "rating", "timestamp"))
 #movies <- str_split_fixed(readLines(unzip(dl, "ml-20m/movies.csv")), "\\::", 3)
 
-#dl <- "~/projects/movielens/ml-1m.zip"   # /!\ MINI dataset
-#ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-1m/ratings.dat"))), col.names = c("userId", "movieId", "rating", "timestamp"))
-#movies <- str_split_fixed(readLines(unzip(dl, "ml-1m/movies.dat")), "\\::", 3)
+dl <- "~/projects/movielens/ml-1m.zip"   # /!\ MINI dataset
+ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-1m/ratings.dat"))), col.names = c("userId", "movieId", "rating", "timestamp"))
+movies <- str_split_fixed(readLines(unzip(dl, "ml-1m/movies.dat")), "\\::", 3)
 ##### FIN A ENLEVER ####
 
 colnames(movies) <- c("movieId", "title", "genres")
@@ -84,7 +84,14 @@ rm(dl, ratings, movies, test_index, temp, movielens, removed)
 
 # Downloading and loading the packages used in this study
 if(!require(dplyr)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(lubridate)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(stringr)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(ggplot2)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+
 library(dplyr)
+library(lubridate)
+library(stringr)
+library(ggplot2)
 
 # Basic data to introduce the global dataset (training + validation)
 total_number_ratings <- nrow(union(edx,validation))
@@ -135,6 +142,41 @@ rmse_naive <- RMSE(validation$rating, mean_rating)
       .$prediction
    # RMSE of the movie/user-effect model
    rmse_movie_user <- RMSE(validation$rating, predictedMU)
+
+# Model that takes into account the year effect
+   # Converting rating timestamp to "y-m-d h:min:s" format
+   edx <- edx %>% 
+      mutate(rating_time = as.POSIXct(timestamp, origin="1970-01-01")) %>%
+      # Extracting year/month of the rating  
+      mutate(rating_year = year(rating_time), rating_month = month(rating_time))
+      # Extracting year of the movie : " (19YY)" or " (20YY)"
+      year_pattern <- "\\s\\(\\d{4}\\)"
+      edx <- edx %>% 
+      mutate(movie_year = str_extract(title,year_pattern)) %>%
+      # Writing correctly the year of the movie
+      mutate(movie_year = as.numeric(str_extract(movie_year, "\\d{4}")))
+
+   # Identifying the most relevant time factor
+      # By movie year (mean rating by movie year)
+      edx <- edx %>% group_by(movie_year) %>% 
+         mutate(RmoyMY = mean(rating)) %>% 
+      # By year span between rating year and movie year (mean rating by delta year)
+         mutate(delta_year = rating_year - movie_year) %>% 
+         group_by(delta_year) %>%
+         mutate(RmoyDY = mean(rating)) %>%
+      # By rating month (mean rating by rating month)
+         group_by(rating_month) %>%
+         mutate(RmoyRM = mean(rating))
+   
+plot_movie_year <- ggplot(edx, aes(y=RmoyMY, x=movie_year)) + 
+   geom_point() +
+   geom_smooth()
+plot_delta_year <- ggplot(edx, aes(y=RmoyDY, x=delta_year)) + 
+   geom_point() +
+   geom_smooth()   
+plot_movie_year <- ggplot(edx, aes(y=RmoyRM, x=rating_month)) + 
+   geom_point() +
+   geom_smooth()
 
 # Save ALL information, for the Rmd report
 save.image (file = "EKR-MovieLens.RData")
